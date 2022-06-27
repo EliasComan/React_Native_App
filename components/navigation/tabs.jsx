e644d1d6cd45a7a969e111672b02260f3fc90cca
EliasComan <comanelias5@gmail.com>
2022-06-27 11:58:59 -0300

    icons

--- components/navigation/tabs.jsx
+++ components/navigation/tabs.jsx
@@ -1,5 +1,8 @@
+import { Text, View } from 'react-native';
+
 import CartNavigation from './cart.navigation'
 import HomeNavigation from './home.navigation'
+import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
@@ -10,8 +13,40 @@ const Tabs = () => {
   return (
     <NavigationContainer >
         <boottomTabs.Navigator initialRouteName='ShopTab'screenOptions={{headerShown:false}} >
-            <boottomTabs.Screen name='ShopTab' component={HomeNavigation}/>
-            <boottomTabs.Screen name='CartShopTab' component={CartNavigation}/>
+            <boottomTabs.Screen 
+                name='ShopTab' 
+                component={HomeNavigation}
+                options={{tabBarLabel:'',
+                tabBarIcon: ({focused}) => (
+                    
+                    <View>
+                      {focused ? 
+                      <Ionicons name="home" size={22} color="black" />
+                      :
+                      <Ionicons name="home-outline" size={22} color="black" />
+                    } 
+                        <Text>Shop</Text>
+                    </View>
+                    )
+            }} 
+            />
+            <boottomTabs.Screen 
+                name='CartShopTab' 
+                component={CartNavigation}
+                options={{tabBarLabel:' ',
+                tabBarIcon: ({focused}) => (
+                    
+                    <View>
+                      {focused ? 
+                      <Ionicons name="cart" size={24} color="black" />
+                      :
+                      <Ionicons name="cart-outline" size={24} color="black" />
+                    } 
+                        <Text>Shop</Text>
+                    </View>
+                    )
+            }}
+            />
         </boottomTabs.Navigator>
     </NavigationContainer>
 
